9041320172394ccf753e63e3a95c9579837b2bc9
larid54 <gurgulpatryk7@gmail.com>
2015-08-13 10:55:26 +0200

    day4 js

--- impl/day4.js
+++ impl/day4.js
@@ -11,6 +11,46 @@
             }
         }
     };
+    getBestStudent: function (students) {
+
+        var iBest = 0;
+        var best = students[iBest].average();
+
+        for (var i = 0; i < students.length; i++) {
+            if (isNaN(students[i].average())) {
+                return false;
+            }
+            if (best < students[i].average()) {
+                iBest = i;
+            }
+        }
+        return students[iBest];
+    }
+    Student.prototype = {
+        addMark: function (mark) {
+            if (mark <= 5 && mark >= 2 && typeof mark !== 'string') {
+                this.marks.push(mark);
+            }
+            else {
+                if (mark instanceof Array) {
+                    for (var i = 0; i < mark.length; i++) {
+                        if (mark[i] <= 5 && mark[i] >= 2 && typeof mark[i] !== 'string') {
+                            this.marks.push(mark[i]);
+                        }
+                    }
+                }
+            }
+            return this.marks;
+        },
+
+        average: function () {
+            var avg = 0;
+            for (var i = 0; i < this.marks.length; i++) {
+                avg += this.marks[i];
+            }
+            return Math.round(avg / this.marks.length * 100) / 100;
+        }
+    };
 
     function Boxes(length, width, weight){
         this.length = length;
@@ -31,13 +71,27 @@
         this.squareBoxes = function(){
             return this.width * this.length;
         };
+        this.transportBoxes = function(car)
+        {
+            if(car.maxSquareTransport>=this.squareBoxes() && car.maxWeightTransport>=this.weight){
+                return 'This car can transport this boxes';
+            }
+            else {
+                return 'This car can\'t transport this boxes';
+            }
+        };
+
     }
-
-
 
     function Car(maxWeightTransport, maxSquareTransport){
         this.maxWeightTransport = maxWeightTransport;
         this.maxSquareTransport = maxSquareTransport;
+    }
+
+    function Student(name, age){
+        this.name = name;
+        this.age = age;
+        this.marks = [];
     }
 
     window.day4 = {
@@ -55,9 +109,7 @@
         car5: new Car(10, 6),
         car6: new Car(50, 20),
 
-        //student1: new Student("Dawid Zegar", 23),
-        //student2: new Student("Jan Kowalski", 77),
-        //student3: new Student("Jacek Nowak", 34)
+
 
         // Place getBestStudent here
 
